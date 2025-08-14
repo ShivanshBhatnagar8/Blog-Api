06944837fc1619ced27f557f421bac4a80fe63cb
shivansh bhatnagar <shivanshbhatnagar8@gmail.com>
2025-08-14 20:58:53 +0530

    fixed the cookie clearing issue

--- Backend/controllers/LoginController.js
+++ Backend/controllers/LoginController.js
@@ -37,7 +37,12 @@ async function loginUser(req, res) {
 
 async function logoutUser(req, res) {
   try {
-    res.cookie("jwt", "", { maxAge: 1 });
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/",
+    });
     res.status(200).json(`Logout successful`);
   } catch (error) {
     return res.status(500).json("Something went Wrong");
